1aeb09a6d3da14cc896b97c67f3acfcd3a4ab411
jihoonLeee <jihoon2723@naver.com>
2023-12-02 23:32:54 +0900

    디자인 빼고 완성 ㅋㅋ

--- controllers/gptController.js
+++ controllers/gptController.js
@@ -13,15 +13,22 @@ exports.getNumbers = async (req, res) => {
     model: "gpt-3.5-turbo",
     messages: [
       {
-        role: "system",
-        content: "내가 지금 느끼는 기분을 한가지 이상 말해주면 1부터 45까지 의 숫자중에 그 기분에 맞춰서 숫자6개만 추천해줄 수 있어? 근거도 포함해서!"
+        "role": "system",
+        "content": "내가 지금 느끼는 기분을 한가지 이상 말해주면 1부터 45까지 의 숫자중에 그 기분에 맞춰서 숫자6개만 추천해줄 수 있어? 근거도 포함해서!"
       },
       {
-        role: "user",
-        content: `내기분은 ${emotions.join(", ")} 이야.  number에 추천숫자를 넣고, reason에 message를 넣어서 JSON 형식으로 알려줘`
+        "role": "assistant",
+        "content": `대답이나 잡답은 절대 하지말고 [{"number": 1, "reason": "블라블라"},{"number": 2, "reason": "블라블라"},{"number": 25, "reason": "블라블라"},{"number": 13, "reason": "블라블라"},{"number": 16, "reason": "블라블라"},{"number": 11, "reason": "블라블라"}] 이 형태로 데이터만 반환해줘`
       },
+      {
+        "role": "user",
+        "content": `내기분은 ${emotions.join(", ")} 이야.`
+      },
+      {
+        "role": "system",
+        "content": "Assistant는 이제 사용자의 감정에 따른 로또 번호와 그 근거를 JSON 형태로 반환해야 합니다. JSON 데이터 이외에 문장은 필요없습니다."
+      }
     ]
   });
-
   res.json( result.choices[0].message.content );
 };
